142eb7722e52919659a56119ee14537cbde07532
𝐑𝑶𝐇𝑰𝑻 𝑺𝑰𝑵𝑮𝑯 <rohitsingh41342@gmail.com>
2024-11-21 01:35:32 +0530

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,17 @@
+package Package1;
+public class Student1{
+private int english;
+private int hindi;
+
+public void setMarks(int p, int q)
+{
+english=p;
+hindi=q;
+
+}
+public void display()
+{
+System.out.println(english);
+System.out.println(hindi);
+}
+}
